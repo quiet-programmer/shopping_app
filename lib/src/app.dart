7939d883144573bf193cd8aced0b60c42add8f93
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-05-05 23:21:13 +0100

    used multi provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_app/screens/home.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app/providers/products_provider.dart';
 import 'package:shopping_app/screens/products_overview_screen.dart';
+import 'package:shopping_app/widgets/cart.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Shopping App",
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.pinkAccent,
-        fontFamily: 'Lato'
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: ProductsProvider(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Shopping App",
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.pinkAccent,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
